7a184b620ec2e0d77fe224a0633c7ac0f0769b8d
2389451262@qq.com <2389451262@qq.com>
2022-07-04 19:40:37 +0800

    chore: clear some config.

--- mirai-ts/config/login.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../types/User'
-
-export const user: User = {
-	qq: 783366159,
-}
-
--- mirai-ts/index.ts
+++ mirai-ts/index.ts
@@ -1,7 +1,6 @@
-import { user } from './config/Login'
 import { ReplyHandler } from './src/handle/ReplyHandler'
 import { Bot } from './src/Instance/Bot'
 
-const bot = new Bot(user)
+const bot = new Bot({ qq: 783366159 })
 bot.use(new ReplyHandler())
 bot.start()
